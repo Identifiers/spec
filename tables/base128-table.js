e1d8880eb01a96acafd5b113319c6feb3d54b983
Matt Bishop <matthew.bishop@elasticpath.com>
2018-03-25 09:21:30 -0700

    added table generator code.

---
+++ tables/base128-table.js
@@ -0,0 +1,46 @@
+const SYMBOLS = "/0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüý";
+const TERMINATOR = "þ";
+
+// creates a Markdown table of the symbols, plus the regexp
+let tbl = `
+### Base-128 Symbol Table
+|pos|char|code|  |pos|char|code|  |pos|char|code|  |pos|char|code|
+|---:|:---:|---:|---|---:|:---:|---:|---|---:|:---:|---:|---|---:|:---:|---:|
+`;
+for (let i = 0; i < SYMBOLS.length / 4; i++) {
+  tbl += `${cell(i)} ${cell(i + 32)} ${cell(i + 64)} ${cell(i + 96)}\n`;
+}
+console.log(tbl);
+
+// regex
+let regexStr = "`^[", startBlock = "", lastChar = "";
+for (let i = 0; i < SYMBOLS.length; i++) {
+  const char = SYMBOLS.charAt(i);
+  if (startBlock === "") {
+    startBlock = char;
+    lastChar = char;
+  } else if (toCharCode(lastChar) + 1 < toCharCode(char)) {
+    //mark a block, start a new one
+    regexStr += `${startBlock}-${lastChar}`;
+    startBlock = char;
+  } else if (i === SYMBOLS.length - 1) {
+    regexStr += `${startBlock}-${char}`;
+  }
+  lastChar = char;
+}
+
+console.log("### Regular Expression");
+regexStr += "]{2,}" + TERMINATOR + "$`";
+console.log(regexStr);
+
+
+// Helper functions
+function toCharCode(char) {
+  return char.charCodeAt(0);
+}
+
+function cell(pos) {
+  const code = SYMBOLS.charCodeAt(pos).toString(16).toUpperCase();
+  const  char = SYMBOLS.charAt(pos);
+  return `|${pos}|**\`${char}\`**|0x${code}|`;
+}
---
+++ tables/base32-table.js
@@ -0,0 +1,78 @@
+/**
+ * Douglas Crockford's base-32 symbols, lowercased.
+ * @see http://www.crockford.com/wrmg/base32.html
+ */
+const SYMBOLS = "0123456789abcdefghjkmnpqrstvwxyz";
+
+/**
+ * Douglas Crockford's base-32 symbol aliases.
+ * @see http://www.crockford.com/wrmg/base32.html
+ */
+const DECODE_ALIASES = {
+  "0": "oO",
+  "1": "iIlL"
+};
+/**
+ * Checksum extra symbols.
+ * @see http://www.crockford.com/wrmg/base32.html
+ */
+const CHECK_EXTRAS = "*~$=u";
+
+// creates a Markdown table of the symbols, plus the checksum addendum
+let tbl = `
+### Base-32 Symbol Table
+|pos|char _[aliases]_|code|  |pos|char _[aliases]_|code|
+|---:|---|---|---|---:|---|---|
+`;
+for (let i = 0; i < SYMBOLS.length / 2; i++) {
+  const cols = [];
+  for (let c = i; c <= i + 16; c += 16) {
+    let chars = SYMBOLS.charAt(c);
+    const aliases = DECODE_ALIASES[chars];
+    if (aliases) {
+      chars += aliases;
+    } else {
+      chars = appendUpperChar(chars);
+    }
+    cols.push(cell(c, chars));
+  }
+  tbl += `${cols[0]} ${cols[1]}\n`;
+}
+
+console.log(tbl);
+
+
+tbl = `### Checksum Addedum
+|pos|char _[alias]_|code|
+|---:|---|---|
+`;
+Array.from(CHECK_EXTRAS).forEach((char, i) => {
+  const chars = appendUpperChar(char);
+  tbl += cell(i + 32, chars) + "\n";
+});
+
+console.log(tbl);
+
+console.log("### Regular Expression");
+console.log("`^_[0-9A-VW-Za-vw-z]{2,}[0-9A-Za-z*~$=]$`");
+
+
+
+// Helper functions
+function cell(pos, chars) {
+  const codes = Array.from(chars).map(char => "0x" + char.charCodeAt(0).toString(16).toUpperCase());
+  let charStr = chars.charAt(0);
+  if (codes.length > 1) {
+    const aliases = Array.from(chars.substr(1));
+    charStr += ` [${aliases.join(",")}]`;
+  }
+  return `|${pos}|**\`${charStr}\`**|${codes.join(", ")}|`;
+}
+
+function appendUpperChar(chars) {
+  const upper = chars.toUpperCase();
+  if (upper != chars) {
+    chars += upper;
+  }
+  return chars;
+};
